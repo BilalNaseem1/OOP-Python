f131e50f4c1ad90266a2c66fd42421b9e8654e7d
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-05-30 15:01:19 +0500

    dict in process

--- 01_procedural_examples/05_Managing_Data_Dicts/05_Bank5_Dictionary.py
+++ 01_procedural_examples/05_Managing_Data_Dicts/05_Bank5_Dictionary.py
@@ -4,10 +4,20 @@ def newAccount(aName, aBalance, aPassword):
     global accountList
     newAccountDict = {
         'name': aName,
-        'balamce': aBalance,
+        'balance': aBalance,
         'password': aPassword
     }
     accountList.append(newAccountDict)
 
 def show(accountNumber):
-    
+    print("Showing the details of the account")
+    thisAccount = accountList[accountNumber]
+    print(f"             Account Name: {thisAccount['name']}")
+    print(f"             Account Balance: {thisAccount['balance']}")
+    print()
+
+
+def getBalance(accountNumber, accountPassword):
+    thisAccount = accountList[accountNumber]
+    if accountPassword == thisAccount['password']:
+
